1f061e8aa08c03a0ed72b70c0e3ab3d7fcd571df
Yusuf-R <y.abdulwasiu@gmail.com>
2023-09-13 20:29:03 +0100

    docs: :memo: import alphabetically

--- models/base.py
+++ models/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Base class for gadgets."""
 
-import uuid
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
+import uuid
 
 
 Base = declarative_base()
