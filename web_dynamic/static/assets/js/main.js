ec5e08829379cd3211371b59c79472c1ce34ed61
saria2016 <saria.m.mohammed@gmail.com>
2023-09-17 16:25:54 +0300

    add slider in index page and setup design

--- web_dynamic/static/assets/js/main.js
+++ web_dynamic/static/assets/js/main.js
@@ -419,3 +419,18 @@ function sendToBackend(searchDict) {
   });
   console.log('Data to be sent to backend:', searchDict);
 }
+
+// slider
+
+var mySwiper = new Swiper(".swiper-container", {
+  direction: "vertical",
+  loop: true,
+  pagination: ".swiper-pagination",
+  grabCursor: true,
+  speed: 1000,
+  paginationClickable: true,
+  parallax: true,
+  autoplay: false,
+  effect: "slide",
+  mousewheelControl: 1
+});
